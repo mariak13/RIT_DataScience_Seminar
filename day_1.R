b727e9c95bdbabed5824041192e314b688effabf
mxs3203 <mxs3203@g.rit.edu>
2020-09-18 12:44:05 +0200

    removing day2 and 3

--- day_1.R
+++ day_1.R
@@ -1,49 +1,65 @@
 
 # Day 1
 
+############  this can be done by going on Files tabs->More->setWorkingDir
 setwd("~/Desktop/RIT_Seminar_Data_Science")
 
-# intro to Rstudio
+############# ############ ############  Intro to Rstudio ############ ############ ############ ############ 
 
-# make folder
+# make folder or navigate where-ever you have your winqualityN.csv (https://www.kaggle.com/rajyellow46/wine-quality)
 
 # make new file
-# go to kaggle and download Wine Quality (https://www.kaggle.com/rajyellow46/wine-quality)
-# school examples in all courses
 
 # on the right you see variables in memory
-# also files, this is where you are, so your path starts from here
-# you can set up your working dir with settings button
+# Also files, this is where you are, so your path starts from here
+# you can set up your working dir with "settings" button
+
 # console 
 
-# global environment
-# click on everything, it will visualize one the left
-# clean it with broom button 
-
-# Plots tab, all of your plots will be here
-# Help page
-?mean(3) 
-
-# connections to DB, not going to use for now
-
+# Running the code
 # running code CMD + Enter
 # also can run a bit of code you selected with mouse
 # or buttons above on the right
+
+mean(1)
+
+# Global environment
+# click on everything, and experiment
+# clean it with broom button 
+
+# Plots tab, all of your plots will be here
+# Help page !!!!
+?mean() 
+
+# every function has a help page which will describe how to use a function
+
+
+
 mean(3) 
 
+# Data types
 # matrix, data.frame, data_frame, list of list
+# numbers, characters, float (number), etc,...
+
+############ ############ ############ Reading data ############ ############ ############ ############ 
+
 ?read.csv
 # returns dataframe
-data <- read.csv("winequalityN.csv", sep = "\t")
-data <- read.csv("winequalityN.csv", sep = ":")
+data <- read.csv("winequalityN.csv", sep = "\t") # every time you run reading the data, check what you actually did
+data <- read.csv("winequalityN.csv", sep = ":") 
 data <- read.csv("winequalityN.csv", sep = " ")
 data <- read.csv("winequalityN.csv", sep = ",")
 data <- read.csv("winequalityN.csv", sep = ",", header = FALSE) 
 data <- read.csv("winequalityN.csv", sep = ",", header = T)
 
+############ ############ ############  Basic Functions ############ ############ ############ ############ 
+
+# always first to do check basic measures of your data
 ?base::summary()
 summary(data)
 
+# NAs in your data
+# what do with them? 
 
 # No NAs
 mean(data$alcohol)
@@ -56,61 +72,114 @@ median(data$volatile.acidity)
 median(data$volatile.acidity, na.rm = T)
 
 sd(data$fixed.acidity, na.rm = T)
+
+# other functions for example
 abs(-2.2)
+sqrt(9)
+# colSums()
+dim(data) # check dimension of your matrix
 
-# dimensions of your data
-dim(data)
+# accessing rows and columns in dataframe
+# data[row_number, column_number_or_name]
 
 
-# accessing specific column
-data[, "pH"]
-data[, 1]
+############# ############ ############  Accessing specific column ############ ############ ############ 
+data[, "pH"] # comma means 'everything', so this is give me all rows for column 'pH'
+data[, 1] # give me everything for first column
 # accessing specific row
-data[55, ]
+data[55, ] # give me all colums for row number 55
 
-data[, "residual.sugar"]
-data[, colnames(data)[5]]
-rownames(data)
+colnames(data) # what are names of my columns?
+rownames(data) # what are names of my rows?
 
-# accessing specific row and column
-data[1, 1]
+# accessing specific row and column (one value)
+data[1, 1] # give me first row, first column value
 
 
 # : represents from-to sequence of numbers
-1:2
-3:4
+1:5
+3:9
+
 # accessing specific rowS and columnS
-data[1:2, 3:4]
+data[1:3, 3:4]
+data[1:3, "pH"] # when we ask for single column such as pH we actaully get a vector (list) of values
 
-# vector 
-col_i_want <- c(1,2,5)
+# vectors
+# I want multiple columns but they are not sequential
+col_i_want <- c(1,2,5) # I make my vector variable
+data[1, col_i_want] # ask for first row and columns 1,2,5
+
+# I can do that by refering to columns by name(becareful about typos) it has to be exactly the same
 col_i_want <- c("residual.sugar","pH")
-colnames(data)
+data[1, col_i_want] # ask for first row and columns residual.sugar and pH
 
-data[1, col_i_want]
 
 
 # data types in R
-typeof(data$fixed.acidity)
+typeof(data$fixed.acidity) # double ,float are decimal numbers
+typeof(data$quality) # integers are rounded numbers
 
 
-# for - loops
-for (i in 1:10){
-  print(i)
-}
+############ ############ ############ ############ Plotting your data and interpreting plots ############ ############ ############ ############ 
 
-# ugly basic R plots
-plot(data$pH, data$density)
+# Let's make few basic R plots
+# Plots go to Plot tab on the right
+# they can be exported, zoomed and resized, deleted, etc
+plot(data$pH, data$density) # x,y scatter plot
+plot(x = data$pH, y = data$density) # x,y scatter plot
+plot(y = data$pH, x = data$density) # x,y scatter plot
+
+# Histograms are plots that you do first to check how is your data distributed
 hist(data$fixed.acidity)
 
 hist(data$residual.sugar, breaks = 200)
 hist(data$alcohol, breaks = 200)
 
+
+# check correlation between two variables
 cor.test(data$pH, data$citric.acid)
 
-ggplot(data = data, aes( x = sulphates, y = pH, color = type) ) + 
+# interpret results
+# coefficient
+# p-value: < 0.05 is statistically significant, > 0.05 is not
+# 2.2e-16 is basically very small number, smallest you can store in single variable
+
+# Installing new packages, it will take 10,15mins
+install.packages("tidyverse")
+install.packages("ggpubr")
+
+# loading them so you can use functions
+library(tidyverse)
+library(ggpubr)
+
+
+# making nice plots
+ggplot(data = data, aes( x = sulphates, y = pH) ) + 
   geom_point() +
-  geom_smooth(method = "lm") + 
-  stat_cor() + 
   theme_minimal() 
+
+############# Done for today
+
+############# For next time
+
+# Try to load your data
+## check if column values are read properly, numbers should be numbers not string "232.1"
+## That often happens if your decimal numbers in file have comma instead of dot (22,3)
+## read.csv() and read.csv2() accepts parameters which can correct for that
+## does your data have header(column names)? you can say header = TRUE or header = FALSE while reading
+## check ?read.csv()   :)
+## check summary
+## check how many NAs
+## print columns 
+## make few basic plots, especially histograms
+## try to interpret data based on histograms
+## make few scatter plots 
+## if you want try ggplot but we are going to work on that on Day 2
+
+
+
+
+
+
+
 
--- day_2.R
+++ day_2.R
@@ -11,56 +11,220 @@ data <- read.csv("winequalityN.csv", sep = ",", header = T)
 library(tidyverse)
 library(ggpubr)
 
-cor.test(data$pH, data$citric.acid)
+# remind ourselves about the data
+summary(data)
 
-ggplot(data = data, aes( x = sulphates, y = pH, color = type) ) + 
+# Let's shape our problem so we know how to approach our data
+# we want to be able to predict alcohol percent from our data
+# let's inspect our data by making plots
+
+# ggplot structure
+ggplot(data = data, aes(x = sulphates , y = pH)) # makes and empty plot with sulphates on x and pH on y
+
+# we have to add a layer to ggplot which will plot the data in any format, we do that by using + 
+# Let's say we want to add points (scatter plot) to the plot
+# geom_point()
+# if you write geom_ and the press tab you can see all other types of visualizations you can make 
+
+ggplot(data = data, aes(x = sulphates , y = pH)) + 
+  geom_point()
+
+?geom_point
+
+# we want to make it a bit nicer so we use theme_minimal
+  
+ggplot(data = data, aes(x = sulphates , y = pH)) + 
   geom_point() +
-  geom_smooth(method = "lm") + 
-  stat_cor() + 
   theme_minimal() 
 
-ggplot(data = data, aes( x = alcohol, y = citric.acid, color = type) ) + 
+# we can have multiple layers on the same plot
+ggplot(data = data, aes(x = sulphates , y = pH)) + 
+  geom_point() + # made points
+  geom_line() + # connected all points with a line
+  theme_minimal()  
+
+?geom_line
+
+# we can remove aes() from ggplot and add it to geom_point()
+# the result is the same
+ggplot(data = data) + 
+  geom_point( aes(x = sulphates , y = pH)) +
+  theme_minimal() 
+
+# but when I add geom_line now, code will get an error because geom_line does not know what to put on lines
+ggplot(data = data) + 
+  geom_point( aes(x = sulphates , y = pH)) +
+  geom_line() + 
+  theme_minimal() 
+
+# so we have to explicitly write the same for lines
+ggplot(data = data) + 
+  geom_point( aes(x = sulphates , y = pH)) +
+  geom_line(aes(x = sulphates , y = pH)) + 
+  theme_minimal() 
+
+# this is usefull if you want to plot multiple variables on the same plot
+# rarely looks good but there is an option..
+ggplot(data = data) + 
+  geom_point( aes(x = sulphates , y = pH)) +
+  geom_line(aes(x = sulphates , y = alcohol)) + 
+  theme_minimal() 
+
+
+# for example maybe a okay reason to plot them on the same plot
+ggplot(data = data) + 
+  geom_point(aes(x = sulphates , y = pH, color = "Sulphate ~ pH")) +
+  geom_point(aes(x = sulphates , y = alcohol, color = "Sulphates ~ alcohol")) + 
+  theme_minimal() 
+
+
+
+# Lets focus on our task: Let's inspect variables in relation to alcohol
+
+
+# it seems there is a clear relationship between those two variables
+ggplot(data = data, aes( x = pH, y = alcohol) ) + 
+  geom_point() +
+  theme_minimal() 
+
+# let's illustrate the relationship
+# we make a line which represents Linear Model that would fit transform X to Y (pH to Alcohol)
+ggplot(data = data, aes( x = pH, y = alcohol) ) + 
+  geom_point() +
+  theme_minimal() +
+  geom_smooth(method = "lm") 
+
+# we can also check correlation between those to variables
+# we see that coeficient is 0.12 (represent steepness of the line) which is not too high
+# but p value is significant which says that those two variables correlate linearly with statistical relevance
+ggplot(data = data, aes( x = pH, y = alcohol) ) + 
+  geom_point() +
+  theme_minimal() +
+  geom_smooth(method = "lm") +
+  stat_cor()
+
+# let's repeat this for other parameters
+
+# it does not seem to be correlated to alcohol
+ggplot(data = data, aes( y = alcohol, x = citric.acid) ) + 
   geom_point() +
   geom_smooth(method = "lm") + 
-  stat_cor() + 
+  stat_cor() +
   theme_minimal()
 
-p1 <- ggplot(data = data, aes( x = type, y = density, fill = type) ) + 
-  geom_boxplot() + 
-  stat_compare_means(method = "t.test") +
+
+# we can also group plots together and visualize them at once
+data$total.sulfur.dioxide
+
+# build them one by one and store them in variable p1,p2,p3,....
+p1 <- ggplot(data = data, aes( x = fixed.acidity, y = alcohol) ) + 
+  geom_point() +
+  geom_smooth(method = "lm") + 
+  stat_cor() +
   theme_minimal()
 
-p2 <- ggplot(data = data, aes( x = type, y = fixed.acidity, fill = type) ) + 
-  geom_boxplot() + 
-  stat_compare_means(method = "t.test") +
+p2 <- ggplot(data = data, aes( x = residual.sugar, y = alcohol) ) + 
+  geom_point() +
+  geom_smooth(method = "lm") + 
+  stat_cor() +
   theme_minimal()
 
-p3 <- ggplot(data = data, aes( x = type, y = chlorides, fill = type) ) + 
-  geom_boxplot() + 
-  stat_compare_means(method = "t.test") +
+p3 <- ggplot(data = data, aes( x = free.sulfur.dioxide, y = alcohol) ) + 
+  geom_point() +
+  geom_smooth(method = "lm") + 
+  stat_cor() +
   theme_minimal()
 
-# we try with t.test and it isignificant but alcohol is not normally dist.
-hist(data$alcohol)
-
-# p4 <- ggplot(data = data, aes( x = type, y = alcohol, fill = type) ) + 
-#   geom_boxplot() + 
-#   stat_compare_means(method = "wilcox") +
-#   theme_minimal()
-
-p4 <- ggplot(data = data, aes( x = type, y = alcohol, fill = type) ) + 
-  geom_boxplot() + 
-  stat_compare_means(method = "wilcox") +
+p4 <- ggplot(data = data, aes( x = total.sulfur.dioxide, y = alcohol) ) + 
+  geom_point() +
+  geom_smooth(method = "lm") + 
+  stat_cor() +
   theme_minimal()
 
 
 ggarrange(p1,p2,p3,p4)
 
+# does color of the wine affect alcohol level?
+# now we are comparing categorical variable to continous
+# so scatterplot won't work so we need boxplots
 
-# we want to make as much as we can plots to have insight 
-#about our variables especially variable we  want to predict
+ggplot(data = data, aes( x = type, y = alcohol, fill = type) ) + 
+  geom_boxplot() +
+  theme_minimal()
+
+# how to check if there is statistical significant difference between groups?
+# t.test or wilcoxon-mann-whitney test
+# t test assumes normal distribution so lets check how is data distributed
+hist(data$alcohol, breaks = 100)
+
+# well not really normally distributed
+ggplot(data = data, aes( x = type, y = alcohol, fill = type) ) + 
+  geom_boxplot() +
+  theme_minimal() +
+  stat_compare_means(method ="wilcox")
+
+# always check the normality of the data because you might draw wrong conclusions
+# according to t-test it is significant
+ggplot(data = data, aes( x = type, y = alcohol, fill = type) ) + 
+  geom_boxplot() +
+  theme_minimal() +
+  stat_compare_means(method ="t.test")
 
 
+summary(data$alcohol)
+# how to interpret boxplots
+# line represents median value, box is middle 50% of your data
+# end of boxes are 25% quartile or 75% quartile
+# lines are datapoints bellow 25% quartile or above 75% quartile
+# points after lines are outliers
+
+# Let's move on
+
+# Let's see if correlations are affected within the color-group
+p1 <- ggplot(data = data, aes( x = fixed.acidity, y = alcohol, color = type) ) + 
+  geom_point() +
+  geom_smooth(method = "lm") + 
+  stat_cor() +
+  theme_minimal()
+
+p2 <- ggplot(data = data, aes( x = residual.sugar, y = alcohol, color = type) ) + 
+  geom_point() +
+  geom_smooth(method = "lm") + 
+  stat_cor() +
+  theme_minimal()
+
+p3 <- ggplot(data = data, aes( x = free.sulfur.dioxide, y = alcohol, color = type) ) + 
+  geom_point() +
+  geom_smooth(method = "lm") + 
+  stat_cor() +
+  theme_minimal()
+
+p4 <- ggplot(data = data, aes( x = total.sulfur.dioxide, y = alcohol, color = type) ) + 
+  geom_point() +
+  geom_smooth(method = "lm") + 
+  stat_cor() +
+  theme_minimal()
+
+
+ggarrange(p1,p2,p3,p4)
+# R will compute statics for each of the colors because it considers it a group
+# in background R will filter the data based on type and perform analysis independently
+
+# It seems that fixed.acidity~ alcohol is slightly differently associated comapring types of wine
+# Most of the red wines are in lower values of residual sugar so it is hard to fit a line 
+# free.sulfur.dioxide is negativley correlated to alohol for red and white wines, slightly more for white
+# Total sulfur.dioxide is very different for red and white wines, most of the data for white is on the right
+# comapred to the red wines which are on the left
+# relationship to alcohol is similar, slightly more negative for white wines
+
+
+# Try to make as much as plots as you can 
+# scatterplots, boxplots, etc
+# focus on alcohol because this is what we want to predict
+# in your dataset you will be focused on co2 or temperature and try to predict it based on other parameters you found
+# keep it simple and understandable
+
+# Next time we will make few more plots and focus on useing that knowledge to make mathematical (predictive) model
 
 
 

bd85e85440c3e9623ecf7d2d503bf38595d22b42
mxs3203 <mxs3203@g.rit.edu>
2020-10-07 22:10:57 +0200

    day 3 step by step

--- day3_climate_change.R
+++ day3_climate_change.R
@@ -8,8 +8,6 @@ split_data <- function(df){
   test = df %>% filter(Year > 2004)
   return(list(train,test))
 }
-
-
 describe_fit <- function(fit){
   cat("Your Model: ", as.character(fit$call), "\n")
   RSS = crossprod(fit$residuals)
@@ -22,7 +20,11 @@ describe_fit <- function(fit){
 
 ############################### We start here, do not change things above ############################
 
+# Change your working directory to RIT_Seminar_Data_Science folder
+
 data <- read.csv("potential_datasets/ClimateChange/climate_change_with_tempC.csv", sep = ",", header = T)
+
+cor.test(train$CO2, train$TempC)
 
 # remove all NAs from data
 data = na.omit(data)
@@ -34,26 +36,104 @@ test = df[[2]] # we will use data from 2004 - 2008 to validate (test) our model
 # Make plots:
 ## N20 - Temp
 
-ggplot(data, aes(x = N2O, y = TempC))+
+ggplot(train, aes(x = N2O, y = TempC))+
   geom_point() +
   stat_cor() + 
   geom_smooth(method = "lm") +
   theme_minimal()
 
 ## CH4 - Temp
+
+ggplot(train, aes(x = CH4, y = TempC)) +
+  geom_point() + 
+  stat_cor() +
+  geom_smooth(method="lm") +
+  theme_minimal()
+
 ## CO2 - Temp
+
+ggplot(train, aes(x = CO2, y = TempC)) +
+  geom_point() + 
+  stat_cor() +
+  geom_smooth(method="lm") +
+  theme_minimal()
+
 ## CFC.11 - Temp
+
+ggplot(train, aes(x = CFC.11, y = TempC)) +
+  geom_point() + 
+  stat_cor() +
+  geom_smooth(method="lm") +
+  theme_minimal()
+
+
 ## MEI - Temp
+
+ggplot(train, aes(x = MEI, y = TempC)) +
+  geom_point() + 
+  stat_cor() +
+  geom_smooth(method="lm") +
+  theme_minimal()
+
+
+
 ## CFC.12-Temp
+
+ggplot(train, aes(x = CFC.12, y = TempC)) +
+  geom_point() + 
+  stat_cor() +
+  geom_smooth(method="lm") +
+  theme_minimal()
+
+
 ## TSI - Temp
+
+ggplot(train, aes(x = TSI, y = TempC)) +
+  geom_point() + 
+  stat_cor() +
+  geom_smooth(method="lm") +
+  theme_minimal()
+
 
 # make LM fit for individual parameter and then adding one by one
 
-# describe RMSE and R^2 for every
+fit = lm(TempC ~ CO2, train)
+summary(fit)
+describe_fit(fit)
+
+
+fit = lm(TempC ~ CO2 + CH4 , train)
+summary(fit)
+describe_fit(fit)
+
+fit = lm(TempC ~ CO2 + N2O , train)
+summary(fit)
+describe_fit(fit)
+
+fit = lm(TempC ~ CO2 + CFC.11 , train)
+summary(fit)
+describe_fit(fit)
+
+fit = lm(TempC ~ CO2 + CFC.12 , train)
+summary(fit)
+describe_fit(fit)
+
+fit = lm(TempC ~ CO2 + CFC.12 + CH4 , train)
+summary(fit)
+describe_fit(fit)
+
+fit = lm(TempC ~ CO2 + CFC.12 + CFC.11 + TSI , train)
+summary(fit)
+describe_fit(fit)
+
+# describe RMSE and R^2 for few models
+
+y_hat = predict(fit, test)
+1/length(y_hat) * (test$TempC - y_hat)^2
 
 # When you think you have the best model, make model with those parameters
 
-# predict on test data and see how much are we off, should be around 2
+# predict on test data and see how much are we off
 
 
 
